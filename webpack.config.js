ef997aa4016054d1b59df33c48cf64ee33e62a83
Erfan Mirzapour <erfanmirzapour1@gmail.com>
2021-04-22 14:34:04 +0430

    Add and config babel
    - Webpack integration
    - Typescript preset

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,24 @@ module.exports = {
    entry: './src/index.ts',
    output: {
       filename: 'main.js',
-      path: path.resolve(__dirname, 'build')
+      path: path.resolve(__dirname, 'build'),
    },
    resolve: {
       extensions: ['ts', 'tsx', 'js'],
+   },
+
+   module: {
+      rules: [
+         {
+            test: /\.tsx?/,
+            include: ['src'],
+            use: {
+               loader: 'babel-loader',
+               options: {
+                  cacheDirectory: true,
+               },
+            },
+         },
+      ],
    },
 };
