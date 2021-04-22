e79b35051d4850a8a8cfeb9389459097c41504f6
Erfan Mirzapour <erfanmirzapour1@gmail.com>
2021-04-22 16:56:11 +0430

    Support older browsers using core-js polyfills

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-   presets: ['@babel/preset-env', '@babel/preset-typescript'],
+   presets: [
+      [
+         '@babel/preset-env',
+         {
+            useBuiltIns: 'usage',
+            corejs: '3.10',
+         },
+      ],
+      '@babel/preset-typescript',
+   ],
 };
