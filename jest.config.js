b46762c3ff2aa280eb5825688bfd79455d7154db
Erfan Mirzapour <erfanmirzapour1@gmail.com>
2021-04-22 19:46:32 +0430

    Configure jest and add test scripts

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+   roots: ['<rootDir>/src'],
+   testEnvironment: 'jest-environment-jsdom',
+   setupFilesAfterEnv: ['<rootDir>/jest/setupTests.ts'],
+   testRegex: '(/__tests__/.+|.+\\.test)\\.tsx?$',
+   // Tests default timeout
+   slowTestThreshold: 3,
+   // Clear mock between tests
+   clearMocks: true,
+
+   // Make jest able to resolve all absolute imports
+   moduleDirectories: ['src', 'node_modules'],
+   moduleFileExtensions: ['js', 'ts', 'tsx', 'json'],
+};
